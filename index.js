90bfdefbb381a3a2f099a43b236eb512efac42ba
Vitalii Vaasylyk <vit.whale@gmail.com>
2023-05-01 13:58:52 +0300

    Initial HW commit

---
+++ index.js
@@ -0,0 +1,8 @@
+// Create a new paragraph element
+var paragraph = document.createElement('p');
+
+// Set the text content of the paragraph element
+paragraph.textContent = 'HELLO WORLDS!';
+
+// Add the paragraph element to the document body
+document.body.appendChild(paragraph);
